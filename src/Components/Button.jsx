fbf8efa40f53c430d86897da701c4b33735656ea
Dhaya3132 <dhayalanu103@gmail.com>
2024-05-18 16:22:01 +0530

    Feat - Added animations

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,5 +1,5 @@
 export function Button({children}) {
     return (
-        <button className="px-4 py-2 border-solid border-2 border-slate-700 rounded-full text-slate-700 w-fit mt-5 hover:bg-green-500 hover:text-white hover:border-none">{children}</button>
+        <button className="px-4 py-2 border-solid border-2 border-slate-700 rounded-full text-slate-700 w-fit mt-5 hover:bg-green-500 hover:text-white hover:border-none animate-bounce">{children}</button>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,27 @@ export default {
         'Purpple': '#392d69',
         'LightPurpple': '#b57bee',
       },
+
       fontFamily: {
         'DancingScript': ['Dancing Script'],
         'Poppins': ['Poppins'],
         'PlayFair': ['PlayFair']
       },
 
+      keyframes: {
+        'move': {
+          '0%': {
+            transform: 'translateX(100%)',
+          },
+          '100%': {
+            transform: 'translateX(-100%)',
+          },
+        },
+      },
+
+      animation: {
+        'Move': 'move 20s linear infinite',
+      },
     },
 
   },
