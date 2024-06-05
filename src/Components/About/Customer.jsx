0635c722ce5e23985aab353107eb65bf648622ec
Dhaya3132 <dhayalanu103@gmail.com>
2024-06-05 18:08:38 +0530

    Feat - Added styles

--- src/Components/About/Customer.jsx
+++ src/Components/About/Customer.jsx
@@ -11,7 +11,7 @@ function Customer() {
     <div className='flex justify-center items-center font-Poppins gap-32'>
       <ScrollTrigger onEnter={() => setEntering(true)} onExit={() => setEntering(false)}>
         <div className='text-center'>
-          <h5 className='font-semibold text-Green text-6xl'>
+          <h5 className='font-semibold text-white text-6xl'>
             {onEntering && <CountUp start={3000} end={4000} delay={0} duration={5} />}+
           </h5>
           <p className='font-medium text-gray-500 text-3xl'>Customer</p>
@@ -19,19 +19,17 @@ function Customer() {
       </ScrollTrigger>
 
       <div className='text-center'>
-        <h5 className='font-semibold text-Green text-6xl'>2023</h5>
+        <h5 className='font-semibold text-white text-6xl'>2023</h5>
         <p className='font-medium text-gray-500 text-3xl'>Best Beauty Salon</p>
       </div>
       <ScrollTrigger onEnter={() => setEntering(true)} onExit={() => setEntering(false)}>
         <div className='text-center'>
-          <h5 className='font-semibold text-Green text-6xl'>
+          <h5 className='font-semibold text-white text-6xl'>
             {onEntering && <CountUp start={2000} end={3750} delay={0} duration={5} />}+
           </h5>
           <p className='font-medium text-gray-500 text-3xl'>Happy Customer</p>
         </div>
       </ScrollTrigger>
-
-
     </div>
   )
 }
